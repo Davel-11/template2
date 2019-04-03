40e0fc3740884ea497aedcea506018a19b60ebfd
Davel-11 <davelaltan@gmail.com>
2019-04-02 22:04:52 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+    ]),
+
+    BsDropdownModule.forRoot(),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
